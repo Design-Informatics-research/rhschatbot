02c752bd87562b55a2a8f8e257be3233678689f5
Roryg <giannichan@gmail.com>
2017-05-04 16:47:13 +0100

    watch position

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,21 @@
+var onSuccess = function(position) {
+  console.log('Latitude: '          + position.coords.latitude          + '\n' +
+  'Longitude: '         + position.coords.longitude         + '\n' +
+  'Altitude: '          + position.coords.altitude          + '\n' +
+  'Accuracy: '          + position.coords.accuracy          + '\n' +
+  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+  'Heading: '           + position.coords.heading           + '\n' +
+  'Speed: '             + position.coords.speed             + '\n' +
+  'Timestamp: '         + position.timestamp                + '\n');
+};
+
+function onError(error) {
+  console.log('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+}
+
+var watchId;
+var geolocationOptions = { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true };
+
 $(document).ready(function(){
 
   var app = {
@@ -18,6 +36,7 @@ $(document).ready(function(){
 
     onDeviceReady: function() {
       this.receivedEvent('deviceready');
+      watchId = navigator.geolocation.watchPosition(onSuccess, onError, geolocationOptions);
     },
 
     receivedEvent: function(id) {
@@ -29,7 +48,6 @@ $(document).ready(function(){
       receivedElement.setAttribute('style', 'display:block;');
       receivedElement.innerHTML = id;
     }
-
   };
 
   app.initialize();
@@ -109,4 +127,3 @@ $(document).ready(function(){
 
   chatbotDb.printLogs();
 });
-
