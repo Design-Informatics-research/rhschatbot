78b6dc59bc5f91161cbc71bfb035505b949ed8b0
Roryg <giannichan@gmail.com>
2017-05-29 15:49:51 +0100

    separate table for allowed patterns

--- www/js/chatbotStorage.js
+++ www/js/chatbotStorage.js
@@ -6,24 +6,39 @@ var chatbotDb = (function () {
 
     migrate: function(){
       db.transaction(function (tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (timestamp unique, text, origin, originName, allowedPatterns)', [], 
+        tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (timestamp unique, text, origin, originName)', [], 
           function(){ console.log("LOGS table created"); }, 
           function(){ console.log("Couldn't created LOGS table"); });
+        tx.executeSql('CREATE TABLE IF NOT EXISTS ALLOWED_PATTERNS (name)', [], 
+          function(){ console.log("ALLOWED_PATTERNS table created"); }, 
+          function(){ console.log("Couldn't created ALLOWED_PATTERNS table"); });
       });
     },
 
     reset: function() {
       db.transaction(function (tx) {
         tx.executeSql("DROP TABLE LOGS", [], 
-          function(){ console.log("Dropped table LOGS, running migrate"); chatbotDb.migrate(); }, 
-          function(){ console.log("Couldn't drop table logs");  });
+          function(){ console.log("Dropped table LOGS"); }, 
+          function(){ console.log("Couldn't drop table LOGS");  });
+        tx.executeSql("DROP TABLE ALLOWED_PATTERNS", [], 
+          function(){ console.log("Dropped table ALLOWED_PATTERNS"); }, 
+          function(){ console.log("Couldn't drop table ALLOWED_PATTERNS");  
+        });
+      }, function(){ console.log("Couldn't reset"); }, function(){ chatbotDb.migrate(); });
+    },
+
+    insertLog: function(text, origin, originName) {
+      db.transaction(function (tx) {
+        tx.executeSql('INSERT INTO LOGS (timestamp, text, origin, originName) VALUES (?, ?, ?, ?)', [new Date-0, text, origin, originName], 
+          function(tx,results){}, 
+          function(tx, error){ console.log(error); } );
       });
     },
 
-    insertLog: function(text, origin, originName, allowedPatterns) {
+    insertAllowedPattern: function(name) {
       db.transaction(function (tx) {
-        tx.executeSql('INSERT INTO LOGS (timestamp, text, origin, originName, allowedPatterns) VALUES (?, ?, ?, ?, ?)', [new Date-0, text, origin, originName, allowedPatterns], 
-          function(tx,results){}, 
+        tx.executeSql('INSERT INTO ALLOWED_PATTERNS (name) VALUES (?)', [name], 
+          function(tx,results){},
           function(tx, error){ console.log(error); } );
       });
     },
@@ -41,6 +56,19 @@ var chatbotDb = (function () {
       });
     },
 
+    allowedPatterns: function(cb){
+      db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM ALLOWED_PATTERNS', [], function (tx, results) {
+          var rows = [];
+          var i;
+          for (i = 0; i < results.rows.length; i++){
+            rows.push(results.rows.item(i).name);
+          }
+          cb(rows);
+        }, null);
+      });
+    },
+
     printLogs: function() {
       chatbotDb.logs(function(rows){
         var i;
@@ -51,6 +79,10 @@ var chatbotDb = (function () {
           console.log(rows[i]);
         }
       });
+    },
+
+    printAllowedPatterns: function() {
+      chatbotDb.allowedPatterns(function(rows){ console.log(rows) });
     }
 
   }
