25197d9054bff3b38f97be49c58698796556f80f
Roryg <giannichan@gmail.com>
2017-06-06 12:16:29 +0100

    adds basic file write code

--- www/js/admin.js
+++ www/js/admin.js
@@ -1,4 +1,37 @@
-//Centre of RHS map in pdf is roughly: 
+var createFile = function(filename){
+  window.requestFileSystem(window.TEMPORARY, 5 * 1024 * 1024, function (fs) {
+    console.log('file system open: ' + fs.name);
+    openFile(fs.root, filename, false);
+  }, function(e){ console.log("Error loading filesystem "+e); });
+};
+
+var openFile = function(dirEntry, fileName, isAppend) {
+  dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
+    writeFile(fileEntry, null, isAppend);
+  }, function(e){ console.log("Error loading filesystem "+e); });
+};
+
+var writeFile = function(fileEntry, dataObj) {
+  fileEntry.createWriter(function (fileWriter) {
+
+    fileWriter.onwriteend = function() {
+      console.log("Successful file write...");
+    };
+
+    fileWriter.onerror = function (e) {
+      console.log("Failed file write: " + e.toString());
+    };
+
+    // If data object is not passed in,
+    // create a new Blob instead.
+    if (!dataObj) {
+      dataObj = new Blob(['some file data'], { type: 'text/plain' });
+    }
+
+    fileWriter.write(dataObj);
+  });
+};
+
 $(document).ready(function(){
 
   var onDeviceReady = function(){
@@ -22,6 +55,11 @@ $(document).ready(function(){
       chatbotDb.reset();
     }
    e.preventDefault();
+  });
+
+  $('#downloadLogs').click(function(e){
+    createFile('hello.txt');
+    e.preventDefault();
   });
 
 });
